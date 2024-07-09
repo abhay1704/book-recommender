2003df4897e455ac2b30150b0c70a00e97620c96
Abhay Pratap Singh <abhaypratapninth@gmail.com>
2024-07-09 12:36:00 +0530

    Bug fixes, added simple caching for front-end and backend

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from packages import create_app
+from dotenv import load_dotenv
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=False, port=5000)
+    load_dotenv()
+    port = int(os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
